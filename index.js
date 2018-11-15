6745a0bed0b8ea54a1d37a8a3caa3e8944bf1fdf
Ronald Olsthoorn <ronaldolsthoorn@gmail.com>
2018-11-15 17:50:45 +0100

    working

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 const { app, BrowserWindow } = require('electron')
-
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
-
-  // and load the index.html of the app.
-  win.loadFile('index.html')
-}
-
-app.on('ready', createWindow)
-Copy
-The main.js should create windows and handle all the system events your application might encounter. A more complete version of the above example might open developer tools, handle the window being closed, or re-create windows on macOS if the user clicks on the app's icon in the dock.
-
-const { app, BrowserWindow } = require('electron')
+const config = require('./config.json')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -20,13 +7,13 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ fullscreen:true, devTools: false, frame: false })
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadURL(config.url)
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
